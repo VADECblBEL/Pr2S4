5d1f84b31042a7abb71608aabcc0308409cfa362
Вадим <vadimmm.m@ya.ru>
2020-09-21 17:25:33 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.IO.IsolatedStorage;
+
+
+namespace Pr2S4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IsolatedStorageFile userStore = IsolatedStorageFile.GetUserStoreForAssembly();
+
+            IsolatedStorageFileStream userStream = new IsolatedStorageFileStream("UserSettings.set", FileMode.Create, userStore);
+            StreamWriter userWriter = new StreamWriter(userStream);
+            userWriter.WriteLine("This text is in file");
+            userWriter.Close();
+
+            string[] files = userStore.GetFileNames("UserSettings.set");
+            if (files.Length == 0)
+            {
+                Console.WriteLine("Oops! Something went wrong X_X");
+            }
+            else
+            {
+                userStream = new IsolatedStorageFileStream("UserSettings.set", FileMode.Open, userStore);
+                StreamReader userReader = new StreamReader(userStream);
+                string contents = userReader.ReadToEnd();
+                 
+                Console.WriteLine(contents);
+                
+            }
+        }
+    }
+}
